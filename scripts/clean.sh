b910f722a9c935664617b4a9090687cacb9f776b
Matteo Rosati <mr2238@login1.mccleary.ycrc.yale.edu>
2025-05-03 16:23:43 -0400

    Fixing input handling

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -9,7 +9,7 @@ fi
 PREFIX="$1"
 
 
-python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'log'  --output_path data/processed/
-python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'yeo-johnson'  --output_path data/processed/
-python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --output_path data/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'log'  --output_path ${PREFIX}/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'yeo-johnson'  --output_path ${PREFIX}/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --output_path ${PREFIX}/processed/
 python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --output_path ${PREFIX}/processed/
