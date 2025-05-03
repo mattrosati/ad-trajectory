6fec6ccf7ccb81c4817b4bf28e59bbc32b0ba807
Matteo Rosati <matteo.rosati98@gmail.com>
2025-05-02 21:07:02 -0400

    Working in both remote and local

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,15 @@
-python ./src/data_clean.py --data_path ~/palmer_scratch/adni/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path data/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'log'  --output_path data/processed/
-python ./src/data_clean.py --data_path ~/palmer_scratch/adni/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path data/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'yeo-johnson'  --output_path data/processed/
-python ./src/data_clean.py --data_path ~/palmer_scratch/adni/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path data/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --output_path data/processed/
-python ./src/data_clean.py --data_path ~/palmer_scratch/adni/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --output_path data/processed/
+#!/bin/bash
+
+# Check if path prefix is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <absolute path to raw data directory>"
+  exit 1
+fi
+
+PREFIX="$1"
+
+
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'log'  --output_path data/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --design_features 'yeo-johnson'  --output_path data/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --vitals_path ${PREFIX}/raw_data.nosync/All_Subjects_VITALS_29Apr2025.csv --output_path data/processed/
+python ./src/data_clean.py --data_path ${PREFIX}/raw_data.nosync/ADNIMERGE/ADNIMERGE_30Apr2025.csv --output_path ${PREFIX}/processed/
